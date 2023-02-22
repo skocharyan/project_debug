37990d7002756b59410a572e95012053cb5ec8a6
SmbatKocharyan <125291623+SmbatKocharyan@users.noreply.github.com>
2023-02-22 12:11:21 +0400

    fix(easyvsl-api): [EVSL-10]  Swagger decorators were added to the missing fields.

--- src/apps/easyvsl/auth/common/dtos/user.login.dto.ts
+++ src/apps/easyvsl/auth/common/dtos/user.login.dto.ts
@@ -3,8 +3,10 @@ import { IsEmail, IsString } from 'class-validator';
 export class UserLoginDto {
   @IsString()
   @IsEmail()
+  @ApiProperty
   email: string;
 
   @IsString()
+  @ApiProperty
   password: string;
 }
